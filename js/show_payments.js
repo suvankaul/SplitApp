5dd00ebe2d54c649c290010fdb43aa70af0129bb
Suvan Kaul <suvankaul97@gmail.com>
2019-07-20 02:32:12 +0530

    display

--- js/show_payments.js
+++ js/show_payments.js
@@ -1,6 +1,7 @@
 function borrowers() {
     var email = document.getElementById("email").value;
     var db = firebase.firestore();
+    document.getElementById("borrowers").innerHTML = "";
     db.collection("users").doc(email).collection("borrower")
         .get()
         .then(function(querySnapshot) {
@@ -9,18 +10,22 @@ function borrowers() {
                 var data = doc.data().amt;
                 console.log(data);
                 var para = document.createElement("p");
-                var data_text = doc.id + " " + data.toString();
+
+                data_text = doc.id + " " + data.toString();
                 var para_text = document.createTextNode(data_text)
                 para.appendChild(para_text);
                 document.getElementById("borrowers").appendChild(para);
+                // data_text = "";
                 console.log(doc.id, doc.data());
             });
         });
+
 }
 
 function lenders() {
     var email = document.getElementById("email").value;
     var db = firebase.firestore();
+    document.getElementById("lenders").innerHTML = "";
     db.collection("users").doc(email).collection("lender")
         .get()
         .then(function(querySnapshot) {
@@ -33,6 +38,7 @@ function lenders() {
                 var para_text = document.createTextNode(data_text)
                 para.appendChild(para_text);
                 document.getElementById("lenders").appendChild(para);
+                data_text = "";
                 console.log(doc.id, doc.data());
             });
         });
