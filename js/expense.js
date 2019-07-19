d5afd11dd3cdec06b9f724cfb3b587936a3691fb
Vishakha Shetye <vishakha.shetye@quantiphi.com>
2019-07-20 01:11:35 +0530

    added update borrow payment functionality

--- js/expense.js
+++ js/expense.js
@@ -16,7 +16,8 @@ function add_expense() {
     var email = document.getElementById("email").value;
     var people = document.getElementById("people").value;
     var amt = document.getElementById("amt").value;
-    var ind_amt = parseInt(amt) / (parseInt(people) + 1);
+    var ind_amt = parseInt(amt) / (parseInt(people)+1);
+    var prev_bal,final_amt;
     //console.log(people);
     var db = firebase.firestore();
     //var add_people = db.collection("users").doc(email).collection('borrowers');
@@ -25,30 +26,41 @@ function add_expense() {
         var e = document.getElementById(i.toString()).value.toString();
         console.log(e);
         //console.log(e.value);
-        add_people.collection("borrower").doc(e).set({
-                amt: ind_amt
+        
+        add_people.collection("borrower").doc(e).get().then(function(queryField) {
+            console.log(queryField.data());
+            if(queryField.data()==undefined){
+                console.log("Its working")
+                prev_bal = 0;
+            }else{
+                console.log(queryField.data());
+                prev_bal = queryField.data().amt;
+            }
+            final_amt = ind_amt + prev_bal;
+            add_people.collection("borrower").doc(e).set({
+                amt: final_amt
             })
             .then(function() {
-                //alert("One record inserted");
+                alert("One record inserted");
                 console.log("Document successfully written!");
                 window.location.href = "index.html";
             })
             .catch(function(error) {
                 console.error("Error writing document: ", error);
             });
-        var lenders = db.collection("users").doc(e);
-        lenders.collection("lender").doc(email).set({
-                amt: ind_amt
-            })
-            .then(function() {
-                alert("One record inserted");
-                console.log("Document successfully written!");
-                // window.location.href = "index.html";
-            })
-            .catch(function(error) {
-                console.error("Error writing document: ", error);
-            });
-
+            var lenders = db.collection("users").doc(e);
+            lenders.collection("lender").doc(email).set({
+                    amt: final_amt
+                })
+                .then(function() {
+                    alert("One record inserted");
+                    console.log("Document successfully written!");
+                    window.location.href = "index.html";
+                })
+                .catch(function(error) {
+                    console.error("Error writing document: ", error);
+                });
+        })
     }
 }
 
@@ -69,28 +81,28 @@ function clear_expense() {
         console.log(remaining_amt);
 
         user.collection("borrower").doc(settle_email).set({
-                amt: remaining_amt
-            })
-            .then(function() {
-                //alert("One record inserted");
-                console.log("Document successfully written!");
-                window.location.href = "index.html";
-            })
-            .catch(function(error) {
-                console.error("Error writing document: ", error);
-            });
+            amt: remaining_amt
+        })
+        .then(function() {
+            alert("One record inserted");
+            console.log("Document successfully written!");
+            window.location.href = "index.html";
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
         var lenders = db.collection("users").doc(settle_email);
         lenders.collection("lender").doc(email).set({
-                amt: remaining_amt
-            })
-            .then(function() {
-                //alert("One record inserted");
-                console.log("Document successfully written!");
-                //window.location.href = "index.html";
-            })
-            .catch(function(error) {
-                console.error("Error writing document: ", error);
-            });
+            amt: remaining_amt
+        })
+        .then(function() {
+            alert("One record inserted");
+            console.log("Document successfully written!");
+            window.location.href = "index.html";
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
 
 
 
