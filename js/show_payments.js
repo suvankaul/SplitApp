874019439655e7ffcbea519dfc6c73bd759c38d8
Suvan Kaul <suvankaul97@gmail.com>
2019-07-20 02:40:27 +0530

    display1

--- js/show_payments.js
+++ js/show_payments.js
@@ -8,15 +8,17 @@ function borrowers() {
             console.log('In function');
             querySnapshot.forEach(function(doc) {
                 var data = doc.data().amt;
-                console.log(data);
-                var para = document.createElement("p");
+                if (data != 0) {
+                    console.log(data);
+                    var para = document.createElement("p");
 
-                data_text = doc.id + " " + data.toString();
-                var para_text = document.createTextNode(data_text)
-                para.appendChild(para_text);
-                document.getElementById("borrowers").appendChild(para);
-                // data_text = "";
-                console.log(doc.id, doc.data());
+                    data_text = doc.id + " " + data.toString();
+                    var para_text = document.createTextNode(data_text)
+                    para.appendChild(para_text);
+                    document.getElementById("borrowers").appendChild(para);
+                    // data_text = "";
+                    console.log(doc.id, doc.data());
+                }
             });
         });
 
@@ -32,14 +34,16 @@ function lenders() {
             console.log('In function');
             querySnapshot.forEach(function(doc) {
                 var data = doc.data().amt;
-                console.log(data);
-                var para = document.createElement("p");
-                var data_text = doc.id + " " + data.toString();
-                var para_text = document.createTextNode(data_text)
-                para.appendChild(para_text);
-                document.getElementById("lenders").appendChild(para);
-                data_text = "";
-                console.log(doc.id, doc.data());
+                if (data != 0) {
+                    console.log(data);
+                    var para = document.createElement("p");
+                    var data_text = doc.id + " " + data.toString();
+                    var para_text = document.createTextNode(data_text)
+                    para.appendChild(para_text);
+                    document.getElementById("lenders").appendChild(para);
+                    data_text = "";
+                    console.log(doc.id, doc.data());
+                }
             });
         });
 }
